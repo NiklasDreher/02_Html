be9cde1952e59df163ffd7366946d9101b2f70fc
NiklasDreher <978fzkz5fr@privaterelay.appleid.com>
2026-02-03 13:45:41 +0100

    Setup_Html_Folder

---
+++ 01_Base/base.js
@@ -0,0 +1,25 @@
+// MyGame - Example implementation of Game interface
+
+class MyGame extends Game {
+  init() {
+    console.log("Game started!");
+  }
+
+  update(deltaTime) {}
+
+  render(ctx) {
+    // ctx.fillStyle = "#da5590";
+    // ctx.arc(100, 100, 20, 0, 2 * Math.PI);
+    // ctx.stroke();
+    // ctx.fillRect(this.x, this.y, 50, 50);
+    // ctx.strokeRect(200, 200, 100, 100);
+    // ctx.font = "48px serif";
+    // ctx.fillText("Hello world", 10, 50);
+    // ctx.fill();
+  }
+}
+
+const game = new MyGame();
+
+const framework = new GameFramework(game, 800, 600);
+framework.start();
